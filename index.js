5a5b1bdc9c28a5a4248a071f2354fd1247ded845
Juju Man <hunteripad2@gmail.com>
2021-01-12 18:10:53 +0400

    Gave access to vercel

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-const fs = require('fs');
+const fs = require("fs");
 let scenariosData;
 fs.readFile("./src/scenariosData.json", (err, data) => {
-	if (err) throw err;
-	scenariosData = data
-})
+    if (err) throw err;
+    scenariosData = data;
+});
+
+const localClient = "http://localhost:3000";
+const vercelClient = "https://great-wars-react.vercel.app/";
+const clientUrl = process.env.dev ? localClient : vercelClient;
 
 app.get("/", (req, res) => {
-	res.set("Access-Control-Allow-Origin", "http://localhost:3000");
-	res.set('Content-Type', 'application/json')
+    res.set("Access-Control-Allow-Origin", clientUrl);
+    res.set("Content-Type", "application/json");
     res.send(scenariosData);
 });
-console.log(port)
+
 app.listen(port);
